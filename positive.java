1fd75fd7f2a5809f51b4669cd22cec65dcea7ce8
thilakas <thilikas29@gmail.com>
2016-07-19 17:56:07 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,22 @@
+package logical1;
+
+import java.util.Scanner;
+
+public class positive {
+public static void main(String[] args) { 
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter a number: ");
+    int number = input.nextInt();
+
+    if( number == 0)
+    { System.out.println("Number is equal to zero"); }
+    else if (number > 0)
+    { System.out.println("Number is positive"); }
+    else 
+    { System.out.println("Number is negative"); }
+
+
+  }
+ }
